3885b35481a04db6cb75e850f8c11052becbdd90
uamadasun <uchennaamadasun@gmail.com>
2024-02-02 14:04:20 -0500

    testing fetch

--- sanity.config.js
+++ sanity.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import schemas from "./sanity/schemas";
 
 const config = defineConfig({
-    projectId: 'p2zi9660',
-    dataset: 'production',
-    title: "Bob Law Firm",
-    apiVersion: '2024-02-02',
-    basePath: '/admin',
-    plugins: []
-})
+  projectId: "p2zi9660",
+  dataset: "production",
+  title: "Bob Law Firm",
+  apiVersion: '2024-02-02',
+  basePath: "/admin",
+  plugins: [structureTool()],
+  schema: { types: schemas },
+  useCdn: true,
+});
 
 export default config;
---
+++ sanity/sanity-utils.js
@@ -0,0 +1,22 @@
+import { createClient, groq } from "next-sanity";
+
+//all the functions we will use to grab data
+export async function getBlogs() {
+  const client = createClient({
+    projectId: "p2zi9660",
+    dataset: "production",
+    apiVersion: "2024-02-02",
+  });
+
+  return await client.fetch(
+    groq`*[_type == "blog"]{
+        _id,
+        _createdAt,
+        name,
+        'slug': slug.current,
+        'image': image.asset->url,
+        url,
+        content
+    }`
+  );
+}
